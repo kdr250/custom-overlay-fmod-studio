06dcdfd5979f768cba9e8611d31ae0ae9b551908
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-05-02 16:32:24 +0900

    hppも追加

--- portfile.cmake
+++ portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 set(SOURCE_PATH $ENV{VCPKG_ROOT}/custom-overlay/fmodstudio)
 
-file(GLOB headers "${SOURCE_PATH}/include/*.h")
+file(GLOB headers "${SOURCE_PATH}/include/*.h" "${SOURCE_PATH}/include/*.hpp")
 file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/fmod")
 
 file(COPY "${SOURCE_PATH}/lib/libFMODStudio.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
