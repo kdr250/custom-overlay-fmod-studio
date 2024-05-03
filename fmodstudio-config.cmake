7f84c3abee1b8a3ae03a99a48847cabd68e71f5f
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-05-03 17:21:13 +0900

    Windows対応

--- fmodstudio-config.cmake
+++ fmodstudio-config.cmake
@@ -2,9 +2,19 @@ add_library(FMODStudio::FMODStudio SHARED IMPORTED)
 get_filename_component(FMODSTUDIO_BASE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)
 
 # add info about location of the dynamic library file
-set_target_properties(
-    FMODStudio::FMODStudio PROPERTIES
-    IMPORTED_IMPLIB "${FMODSTUDIO_BASE_DIR}/lib/libFMODStudio.dylib"
-)
+if (WIN32)
+    cmake_path(SET FMODSTUDIO_DLL_PATH NORMALIZE ${FMODSTUDIO_BASE_DIR}/bin/fmodstudio.dll)
+    cmake_path(SET FMODSTUDIO_LIB_PATH NORMALIZE ${FMODSTUDIO_BASE_DIR}/lib/fmodstudio_vc.lib)
+    set_target_properties(
+        FMODStudio::FMODStudio PROPERTIES
+        IMPORTED_LOCATION "${FMODSTUDIO_DLL_PATH}"
+        IMPORTED_IMPLIB "${FMODSTUDIO_LIB_PATH}"
+    )
+elseif (APPLE)
+    set_target_properties(
+        FMODStudio::FMODStudio PROPERTIES
+        IMPORTED_IMPLIB "${FMODSTUDIO_BASE_DIR}/lib/libfmodstudio.dylib"
+    )
+endif()
 
 target_include_directories(FMODStudio::FMODStudio INTERFACE "${FMODSTUDIO_BASE_DIR}/include")
--- portfile.cmake
+++ portfile.cmake
@@ -1,11 +1,16 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-set(SOURCE_PATH $ENV{VCPKG_ROOT}/custom-overlay/fmodstudio)
-
-file(GLOB headers "${SOURCE_PATH}/include/*.h" "${SOURCE_PATH}/include/*.hpp")
+file(GLOB headers "${CMAKE_CURRENT_LIST_DIR}/include/*.h" "${CMAKE_CURRENT_LIST_DIR}/include/*.hpp" "${CMAKE_CURRENT_LIST_DIR}/include/*.cs")
 file(COPY ${headers} DESTINATION "${CURRENT_PACKAGES_DIR}/include/fmod")
 
-file(COPY "${SOURCE_PATH}/lib/libFMODStudio.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-file(COPY "${SOURCE_PATH}/fmodstudio-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+if (WIN32)
+    file(COPY "${CMAKE_CURRENT_LIST_DIR}/lib/fmodstudio.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+    file(COPY "${CMAKE_CURRENT_LIST_DIR}/lib/fmodstudio_vc.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+elseif (APPLE)
+    file(COPY "${CMAKE_CURRENT_LIST_DIR}/lib/libfmodstudio.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+endif()
 
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/fmodstudio-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
